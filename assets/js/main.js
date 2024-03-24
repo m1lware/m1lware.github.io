5a6147e8b6cb575f46adbebd9129d1189504e67c
nawaf <91866115+m1lware@users.noreply.github.com>
2024-03-24 05:26:54 +0300

    Update main.js
    adjust the canvas size dynamically for screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+
+//This is code Writed by Nawaf, and you're not allowed to copy it :)
+// Text Rotation Function
 var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -6,103 +9,102 @@ var TxtRotate = function(el, toRotate, period) {
     this.txt = '';
     this.tick();
     this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
+};
+
+TxtRotate.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
-  
+
     if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-  
+
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
+
     var that = this;
     var delta = 300 - Math.random() * 100;
-  
+
     if (this.isDeleting) { delta /= 2; }
-  
+
     if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
+        delta = this.period;
+        this.isDeleting = true;
     } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
     }
-  
+
     setTimeout(function() {
-      that.tick();
+        that.tick();
     }, delta);
-  };
-  
-  window.onload = function() {
+};
+
+// Initialization of Text Rotation
+window.onload = function() {
     var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
     }
-    // INJECT CSS
+
+    // Style Injection for Text Rotation
     var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
-  };
 
+    // Call to resize and draw canvas
+    resizeCanvas();
+    draw();
+};
+
+// Canvas Resizing and Digital Rain
 var c = document.getElementById("c");
-if( c ){
-    var ctx = c.getContext("2d");
+var ctx = c.getContext("2d");
+var chinese = "01".split("");
+var font_size = 10;
+var drops = [];
+var columns;
 
-    //making the canvas full screen
-
-    c.height = window.innerHeight;
+function resizeCanvas() {
     c.width = window.innerWidth;
-
-    //chinese characters - taken from the unicode charset
-    var chinese = "01";
-    //converting the string into an array of single characters
-    chinese = chinese.split("");
-
-    var font_size = 10;
-    var columns = c.width / font_size; //number of columns for the rain
-    //an array of drops - one per column
-    var drops = [];
-    //x below is the x coordinate
-    //1 = y co-ordinate of the drop(same for every drop initially)
-    for (var x = 0; x < columns; x++)
-        drops[x] = 1;
-
-    //drawing the characters
-    function draw() {
-        //Black BG for the canvas
-        //translucent BG to show trail
-        ctx.fillStyle = "rgba(33, 37, 41, .2)";
-        ctx.fillRect(0, 0, c.width, c.height);
-
-        ctx.fillStyle = "rgba(255,255,255,.3)"; //green text
-        ctx.font = font_size + "px arial";
-        //looping over drops
-        for (var i = 0; i < drops.length; i++) {
-            //a random chinese character to print
-            var text = chinese[Math.floor(Math.random() * chinese.length)];
-            //x = i*font_size, y = value of drops[i]*font_size
-            ctx.fillText(text, i * font_size, drops[i] * font_size);
-
-            //sending the drop back to the top randomly after it has crossed the screen
-            //adding a randomness to the reset to make the drops scattered on the Y axis
-            if (drops[i] * font_size > c.height && Math.random() > 0.975)
-                drops[i] = 0;
-
-            //incrementing Y coordinate
-            drops[i]++;
-        }
-    }
-
-    setInterval(draw, 33);
+    c.height = window.innerHeight;
+    columns = c.width / font_size;
+    drops = [];
+    for (var x = 0; x < columns; x++) drops[x] = 1;
 }
+
+// Digital Rain Drawing Function
+function draw() {
+    ctx.fillStyle = "rgba(33, 37, 41, .2)";
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "rgba(255,255,255,.3)";
+    ctx.font = font_size + "px arial";
+    for (var i = 0; i < drops.length; i++) {
+        var text = chinese[Math.floor(Math.random() * chinese.length)];
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+        if (drops[i] * font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        drops[i]++;
+    }
+}
+
+// Adjust canvas size when window is resized
+window.onresize = function() {
+    resizeCanvas();
+    // Optionally, clear the canvas and reset the drops when resizing
+    // This avoids visual glitches during resizing
+    draw(); 
+};
+
+// Start the animation loop
+setInterval(draw, 33);
